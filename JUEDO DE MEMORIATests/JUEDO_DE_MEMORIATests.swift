183da9f8043acef587ee06b8ca6fdfa79a11a58c
Jenny <jenny@jennys-mac-pro.local>
2015-11-25 19:41:06 -0800

    Initial Commit

---
+++ JUEDO DE MEMORIATests/JUEDO_DE_MEMORIATests.swift
@@ -0,0 +1,36 @@
+//
+//  JUEDO_DE_MEMORIATests.swift
+//  JUEDO DE MEMORIATests
+//
+//  Created by Jenny on 25/11/15.
+//  Copyright © 2015 Jenny. All rights reserved.
+//
+
+import XCTest
+@testable import JUEDO_DE_MEMORIA
+
+class JUEDO_DE_MEMORIATests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
